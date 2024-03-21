b5ae877fe17696978f8aeacfbbbef106abb1542a
ogzCode <oguzguc44@gmail.com>
2024-03-21 10:22:10 +0300

    Refactor auth routes and add exception handling

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -10,12 +10,14 @@ router = APIRouter(
     tags=["Auth"]
 )
 
+
 def validate_password(user: AuthSchema):
     if len(user.password) < 6:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Password must be at least 6 characters"
         )
+
 
 @router.post("/register", response_model=CustomResponse)
 async def register_user(user: RegisterSchema, db: Session = Depends(db.get_session)):
@@ -24,44 +26,41 @@ async def register_user(user: RegisterSchema, db: Session = Depends(db.get_sessi
             new_user = UserServices().create_user(db, user, "admin")
         else:
             new_user = UserServices().create_user(db, user)
-    except Exception as e:
-        print(e)
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=str(e)
-        )
-    print(new_user)
-    return CustomResponse(
-        message="User created successfully",
-        data={
-            "user": new_user
-        },
-        status=201
-    )
 
-@router.post("/login", response_model=CustomResponse, dependencies=[Depends(validate_password)])
+        return CustomResponse(
+            message="User created successfully",
+            data={
+                "user": new_user
+            },
+            status=201
+        )
+    except HTTPException as e:
+        raise e
+
+
+@router.post("/login", dependencies=[Depends(validate_password)])
 async def login_user(user: AuthSchema, db: Session = Depends(db.get_session)):
     try:
         db_user = UserServices().get_user_by_email(db, user.email)
-        if not UserServices().verify_user_password(db, user.email, user.password):
+
+        if not db_user or not UserServices().verify_user_password(db, user.email, user.password):
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Invalid credentials"
             )
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=str(e)
+
+        token = token_services.create_access_token({
+            "id": db_user.id,
+            "role": db_user.role
+        })
+        return CustomResponse(
+            message="User logged in successfully",
+            data={
+                "token": token,
+                "user": db_user
+            },
+            status=200
         )
-    token = token_services.create_access_token({
-        "id": db_user.id,
-        "role": db_user.role
-    })
-    return CustomResponse(
-        message="User logged in successfully",
-        data={
-            "token": token,
-            "user": db_user
-        },
-        status=200
-    )
+    except HTTPException as e:
+        print(e)
+        raise e
